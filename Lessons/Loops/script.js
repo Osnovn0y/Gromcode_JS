b4ffb3602b4f7238115d4742d12f8d452a2808ea
Osnovn0y <130398559+Osnovn0y@users.noreply.github.com>
2024-07-13 11:58:34 +0400

    fix

---
+++ Lessons/Loops/script.js
@@ -0,0 +1,31 @@
+// Sum of numbers
+
+// let n = 150;
+// let sum = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   sum += i;
+//   console.log(sum);
+// }
+
+// Multiplication
+
+// let m = 10;
+// const n = 20;
+// let result = 1;
+
+// for (let i = m; i <= n; i++) {
+//   if (i % 2 !== 0) {
+//     result *= i;
+//   }
+// }
+// console.log("Result: ", result);
+
+// Multiplication table
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log("\n=> " + i + " <=\n\n");
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(i + " x " + j + " = " + i * j);
+//   }
+// }
